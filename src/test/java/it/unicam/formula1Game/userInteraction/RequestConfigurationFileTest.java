1fd46e1e8d413e64dc47b8dd0d118d19fca69e22
iacopocascia <iacopo.cascia@studenti.unicam.it>
2025-01-26 18:38:37 +0100

    Fix RequestConfigurationFileTest bug

--- src/test/java/it/unicam/formula1Game/userInteraction/RequestConfigurationFileTest.java
+++ src/test/java/it/unicam/formula1Game/userInteraction/RequestConfigurationFileTest.java
@@ -5,24 +5,16 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
-import java.nio.file.NoSuchFileException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RequestConfigurationFileTest {
     @Test
-    public void request_not_existing_configuration_file_test() {
-        String simulatedInput = "src/jsonRaceTracks/notExisting.json";
-        InputStream in = new ByteArrayInputStream(simulatedInput.getBytes());
-        System.setIn(in);
-        assertThrows(NoSuchFileException.class, RequestConfigurationFile::requestConfigurationFile);
-    }
-    @Test
-    public void request_existing_configuration_file_test() throws NoSuchFileException {
+    public void request_existing_configuration_file_test() {
         String simulatedInput = "src/jsonRaceTracks/track.json";
         InputStream in = new ByteArrayInputStream(simulatedInput.getBytes());
         System.setIn(in);
-        assertEquals(RequestConfigurationFile.requestConfigurationFile().getClass(),File.class);
+        assertEquals(RequestConfigurationFile.requestConfigurationFile().getClass(), File.class);
     }
 }
 
